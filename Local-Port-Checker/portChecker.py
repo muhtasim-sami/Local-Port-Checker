a6d06e2b369df0cd14b21f715a1dbf1ea8f55e60
muhtasim-sami <muhtasimsami2002@gmail.com>
2025-08-30 00:36:22 +0600

    create local port checker

---
+++ Local-Port-Checker/portChecker.py
@@ -0,0 +1,16 @@
+import requests, subprocess as sb
+cmd = "Get-NetTCPConnection -State Listen  | select -ExpandProperty LocalPort | sort -Unique"
+
+def shell(cmd):
+    return sb.run(["powershell","-Command",cmd],capture_output=True, text=True)
+
+ports = shell(cmd).stdout.split(sep='\n')
+print("open ports are : \n", ports)
+
+for port in ports:
+    try:
+        response = requests.get(f"http://127.0.0.1:{port}/apex", timeout=5)
+        if response.headers['Server'] == 'Oracle XML DB/Oracle Database':
+            print(f"Port {port} is open")
+    except :
+        pass
